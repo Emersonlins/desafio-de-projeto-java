6c650ce02eed3be2d5e782c5a02fc255acc21785
EMERSON <106836886+Emersonlins@users.noreply.github.com>
2022-08-01 14:35:30 -0300

    Update ContaPoupanca.java

--- src/main/java/exemplo/banco/desafiodeprojetojava/ContaPoupanca.java
+++ src/main/java/exemplo/banco/desafiodeprojetojava/ContaPoupanca.java
@@ -1,2 +1,19 @@
-package exemplo.banco.desafiodeprojetojava;public class ContaPoupanca {
+package exemplo.banco.desafiodeprojetojava;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void transferir(double valor, Conta contadestino) {
+
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        super.imprimirInfosComuns();
+    }
 }
