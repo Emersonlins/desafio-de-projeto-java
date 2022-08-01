5e7ee6c648cb0df09e8fa8af349a098648eda318
EMERSON <106836886+Emersonlins@users.noreply.github.com>
2022-08-01 14:34:57 -0300

    Update ContaCorrente.java

--- src/main/java/exemplo/banco/desafiodeprojetojava/ContaCorrente.java
+++ src/main/java/exemplo/banco/desafiodeprojetojava/ContaCorrente.java
@@ -1,2 +1,20 @@
-package exemplo.banco.desafiodeprojetojava;public class ContaCorrente {
+package exemplo.banco.desafiodeprojetojava;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void transferir(double valor, Conta contadestino) {
+
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+
 }
