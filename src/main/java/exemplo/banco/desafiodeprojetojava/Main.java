988426ff51f8104dbea85c24622773e75b87a822
EMERSON <106836886+Emersonlins@users.noreply.github.com>
2022-08-01 14:36:21 -0300

    Update Main.java

--- src/main/java/exemplo/banco/desafiodeprojetojava/Main.java
+++ src/main/java/exemplo/banco/desafiodeprojetojava/Main.java
@@ -1,2 +1,18 @@
-package exemplo.banco.desafiodeprojetojava;public class Main {
+package exemplo.banco.desafiodeprojetojava;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente Emerson = new Cliente();
+        Emerson.setNome("Emerson");
+
+        Conta cc = new ContaCorrente(Emerson);
+        Conta poupanca = new ContaPoupanca(Emerson);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
 }
